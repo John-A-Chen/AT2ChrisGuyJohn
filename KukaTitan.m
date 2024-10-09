3416c719b2a9f881bc29498217874ece47f6509c
John-A-Chen <cohnjhen@gmail.com>
2024-10-09 15:08:58 +1100

    Setting up my github desktop

--- KukaTitan.m
+++ KukaTitan.m
@@ -10,18 +10,22 @@ classdef KukaTitan < RobotBaseClass
             if nargin < 1
                 baseTr = eye(4);
             end
-            self.model.base = self.model.base.T * baseTr * trotx(pi/2) * troty(pi/2);
-            self.PlotAndColourRobot();
+            self.model.base = self.model.base.T * baseTr;
+            self.model.plot(zeros(6));
+            hold on
+            % PlaceObject('KUKATitanLink0.PLY');
+            % PlaceObject('KUKATitanLink1.PLY');
         end
         %% CreateModel
         function CreateModel(self)
-            link(1) = Link('d',0,   'a',0,   'alpha',0,   'offset',0,   'qlim',deg2rad([-150,150]));
-            link(2) = Link('d',0,   'a',0,   'alpha',0,   'offset',0,   'qlim',deg2rad([-130,17.5]));
-            link(3) = Link('d',0,   'a',0,   'alpha',0,   'offset',0,   'qlim',deg2rad([-110,145]));
-            link(4) = Link('d',0,   'a',0,   'alpha',0,   'offset',0,   'qlim',deg2rad([-350,350]));
-            link(5) = Link('d',0,   'a',0,   'alpha',0,   'offset',0,   'qlim',deg2rad([-118,118]));
-            link(6) = Link('d',0,   'a',0,   'alpha',0,   'offset',0,   'qlim',deg2rad([-350,350]));
+            link(1) = Link('d',1100,   'a',600,   'alpha',-pi/2,   'offset',0,   'qlim',deg2rad([-150,150]));
+            link(2) = Link('d',0,   'a',1465,   'alpha',0,   'offset',0,   'qlim',deg2rad([-130,17.5]));
+            link(3) = Link('d',0,   'a',0,   'alpha',pi/2,   'offset',0,   'qlim',deg2rad([-110,145]));
+            link(4) = Link('d',1600,   'a',0,   'alpha',-pi/2,   'offset',0,   'qlim',deg2rad([-350,350]));
+            link(5) = Link('d',0,   'a',0,   'alpha',pi/2,   'offset',0,   'qlim',deg2rad([-118,118]));
+            link(6) = Link('d',372,   'a',0,   'alpha',0,   'offset',0,   'qlim',deg2rad([-350,350]));
             self.model = SerialLink(link, 'name', self.name);
+            axis equal;
         end
     end
 end
---
+++ lmao.m
@@ -0,0 +1,3 @@
+PlaceObject('KUKATitanLink0.ply',      [0,     0.75,  -0.965]);
+axis equal
+lighting gouraud
