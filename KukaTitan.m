af6d87eaaa22612430c51a6c096ba447280829f6
Chris-3000 <dogsonly734@gmail.com>
2024-10-14 16:16:02 +1100

    All links correctly adjusted in space

--- KukaTitan.m
+++ KukaTitan.m
@@ -13,7 +13,8 @@ classdef KukaTitan < RobotBaseClass
             self.model.base = self.model.base.T * baseTr;
             % self.model.plot(zeros(6));
             self.PlotAndColourRobot();
-            axis([-5, 5, -5, 5, -1, 5]);
+            % axis([-10, 10, -10, 10, -10, 10]);
+            axis([-5, 5, -5, 5, -1, 6]);
             % hold on
             % PlaceObject('KUKATitanLink0.PLY');
             % PlaceObject('KUKATitanLink1.PLY');
@@ -22,8 +23,8 @@ classdef KukaTitan < RobotBaseClass
         function CreateModel(self)
             L(1) = Link('d', 1.1, 'a', 0.6, 'alpha', -pi/2, 'qlim', [-150, 150] * pi/180);
             L(2) = Link('d', 0, 'a', 1.465, 'alpha', 0, 'offset', -pi/2, 'qlim', [-40, 107.5] * pi/180);
-            L(3) = Link('d', 0, 'a', 0, 'alpha', pi/2, 'offset', pi, 'qlim', [-110, 145] * pi/180);
-            L(4) = Link('d', 1.6, 'a', 0, 'alpha', -pi/2, 'qlim', [-350, 350] * pi/180);
+            L(3) = Link('d', 0, 'a', 0, 'alpha', pi/2, 'offset', pi, 'qlim', [-110 - 90, 145 - 90] * pi/180);
+            L(4) = Link('d', 1.2, 'a', 0, 'alpha', -pi/2, 'qlim', [-350, 350] * pi/180);
             L(5) = Link('d', 0, 'a', 0, 'alpha', pi/2, 'qlim', [-118, 118] * pi/180);
             L(6) = Link('d', 0.372, 'a', 0, 'alpha', 0, 'qlim', [-350, 350] * pi/180);
             self.model = SerialLink(L, 'name', self.name);
