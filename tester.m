92cf4c64cc3107aa867706b8d67f377606258573
John-A-Chen <cohnjhen@gmail.com>
2024-10-22 20:21:34 +1100

    some changes

--- tester.m
+++ tester.m
@@ -8,4 +8,8 @@ PlaceObject('UR3eLink3a.PLY',[-0.24355-0.2132,0,0.15185]);
 PlaceObject('UR3eLink4a.PLY',[-0.24355-0.2132,-0.11235,0.15185]);
 PlaceObject('UR3eLink5a.PLY',[-0.24355-0.2132,-0.11235,0.15185-0.08535]);
 PlaceObject('UR3eLink6a.PLY',[-0.24355-0.2132,-0.11235-0.0819,0.15185-0.08535]);
+
+PlaceObject('Sensors and control mount v2.PLY',[0,0,0]);
+PlaceObject('IR_PenMount v5.PLY',[0,0.5,0]);
+PlaceObject('IR_PenTube v6.PLY',[0,0.5,0]);
 axis equal;
